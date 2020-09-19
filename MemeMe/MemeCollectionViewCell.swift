200fd11e2953f736afccc6239a07d8a727f2d046
asapkent <jeffersalex94@gmail.com>
2020-09-19 16:44:43 -0400

    add button to acmes send meme vc from two screens.

---
+++ MemeMe/MemeCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  MemeCollectionViewCell.swift
+//  MemeMe
+//
+//  Created by Robert Jeffers on 9/15/20.
+//  Copyright © 2020 AsapInc. All rights reserved.
+//
+
+import UIKit
+
+class MemeCollectionViewCell: UICollectionViewCell {
+   
+    
+    @IBOutlet weak var memeiamge: UIImageView!
+}
